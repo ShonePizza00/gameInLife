02984f558119649d4fe5920f51383bf4e192b0f1
ShonePizza00 <93098865+ShonePizza00@users.noreply.github.com>
2024-01-30 18:22:40 +0300

    Update graphicsEngine.cpp

--- gameInLife/graphicsEngine.cpp
+++ gameInLife/graphicsEngine.cpp
@@ -6,11 +6,17 @@ graphicsEngine::graphicsEngine(unsigned int x, unsigned int y)
 	size_y = y;
 	width = x * PIXEL_SIZE_X;
 	height = y * PIXEL_SIZE_Y;
+	colors = new cv::vec3b[5];
+	colors[0] = cv::Vec3b(0, 0, 0);
+	colors[1] = cv::Vec3b(255, 0, 0);
+	colors[2] = cv::Vec3b(0, 255, 0);
+	colors[3] = cv:Vec3b(0, 0, 255);
+	colors[4] = cv::Vec3b(255, 255, 255);
 }
 
 graphicsEngine::~graphicsEngine()
 {
-	return;
+	delete[] colors;
 }
 
 void graphicsEngine::update(bool** dispRows)
@@ -21,23 +27,17 @@ void graphicsEngine::update(bool** dispRows)
 	{
 		for (int x = 0; x < size_x; ++x)
 		{
-			cv::Vec3b t1;
-			if (dispRows[y][x])
+			for (int iy = 0; iy < PIXEL_SIZE_Y; ++iy)
 			{
-				/*t1[0] = color[0];
-				t1[1] = color[1];
-				t1[2] = color[2];
-				*/
-				for (int iy = 0; iy < PIXEL_SIZE_Y; ++iy)
+				for (int ix = 0; ix < PIXEL_SIZE_X; ++ix)
 				{
-					for (int ix = 0; ix < PIXEL_SIZE_X; ++ix)
-					{
-						screen.at<cv::Vec3b>(y * PIXEL_SIZE_Y + iy, x * PIXEL_SIZE_X + ix) = color;
-					}
+					screen.at<cv::Vec3b>(y * PIXEL_SIZE_Y + iy, x * PIXEL_SIZE_X + ix) = colors[dispRows[y][x]];
 				}
+			}
 			}
 		}
 	}
+	/*
 	if (color[0] == 255 && color[2] == 0 && color[1] != 255)
 		++color[1];
 	else if (color[0] == 255 && color[2] > 0)
@@ -50,6 +50,7 @@ void graphicsEngine::update(bool** dispRows)
 		--color[1];
 	else if (color[2] == 255 && color[1] == 0 && color[0] != 255)
 		++color[0];
+	*/
 	cv::imshow("Game in Life", screen);
 	cv::waitKey(1);
 	return;
